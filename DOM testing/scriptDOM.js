dce7a7a42b2d2564f644d1a1c1cf6641a3a2941d
Z <161516388+Ziphour@users.noreply.github.com>
2024-04-23 18:19:34 +0100

    Button working to start game

--- DOM testing/scriptDOM.js
+++ DOM testing/scriptDOM.js
@@ -1,5 +1,24 @@
-  function startGameonButton() {
-    /*put form function in here*/
+
+const Gamecontroller = (function () {
+  players = [{
+    Username: `p1`,
+    marker: 1,
+    markerImg: 'o.png',
+    score: 0,
+
+  },
+
+  {
+    Username: `p2`,
+    marker: 2,
+    markerImg: 'x.png',
+    score: 0
+
+
+  }]
+
+  const makeBoard = function () {
+
     Gameboard = []
     for (i = 0; i < 3; i++) {
       Gameboard[i] = []
@@ -8,192 +27,164 @@
       }
     }
 
+  }
+
+  const startGame = function () {
+    makeBoard()
     alert(`Its ${Gamecontroller.activeplayer.Username}'s go!`)
-    // Gamecontroller.startGame()
-    // need some way of stopping and starting the game 
+    screenSelector.allowClicks()
   }
 
-  const Gamecontroller = (function () {
-    players = [{
-      Username: `p1`,
-      marker: 1,
-      markerImg: 'o.png',
-      score: 0,
+  const winConditionList = [
 
-    },
+    [[0, 0], [0, 1], [0, 2]],
+    [[1, 0], [1, 1], [1, 2]],
+    [[2, 0], [2, 1], [2, 2]],
+    [[0, 0], [1, 0], [2, 0]],
+    [[0, 1], [1, 1], [2, 1]],
+    [[0, 2], [1, 2], [2, 2]],
+    [[0, 0], [1, 1], [2, 2]],
+    [[2, 0], [1, 1], [0, 2]]
 
-    {
-      Username: `p2`,
-      marker: 2,
-      markerImg: 'x.png',
-      score: 0
+  ]
 
 
-    }]
+  const winCheck = function () {
 
+    for (j = 0; j < winConditionList.length; j++) {
 
-    const winConditionList = [
+      let gameBoardTokens = winConditionList[j].map(checkTokens)
 
-      [[0, 0], [0, 1], [0, 2]],
-      [[1, 0], [1, 1], [1, 2]],
-      [[2, 0], [2, 1], [2, 2]],
-      [[0, 0], [1, 0], [2, 0]],
-      [[0, 1], [1, 1], [2, 1]],
-      [[0, 2], [1, 2], [2, 2]],
-      [[0, 0], [1, 1], [2, 2]],
-      [[2, 0], [1, 1], [0, 2]]
-
-    ]
-
-
-    const winCheck = function () {
-
-      for (j = 0; j < winConditionList.length; j++) {
-
-        let gameBoardTokens = winConditionList[j].map(checkTokens)
-
-        function checkTokens(coordinates) {
-          let [row, col] = coordinates
-          return Gameboard[row][col].getvalue()
-
-        }
-
-        const checkWin = gameBoardTokens.every(value => value === activeplayer.marker)
-        if (checkWin === true) {
-          alert(`congrats ${activeplayer.Username}! 🥳`)
-          activeplayer.score++
-          throw new Error("End's the game!");
-        }
-
-      }
-    }
-
-    const startGame = () => {
-
-      for (i = 0; i < 9; i++) {
-
-        playRound(coordinates)
-        winCheck()
-        newTurn()
-
+      function checkTokens(coordinates) {
+        let [row, col] = coordinates
+        return Gameboard[row][col].getvalue()
       }
 
-      alert(`No one won, tie! try again`);
+      const checkWin = gameBoardTokens.every(value => value === activeplayer.marker)
 
-      // is this the best way to use a bbutton mechanic
-      // while loop?
-    }
-
-    const playRound = function (e) {
-
-      // need this used before click
-      droptkn(e)
-      screenSelector.markeranimationsX(e,activeplayer)
-      winCheck()
-      newTurn()
+      if (checkWin === true) {
+        alert(`congrats ${activeplayer.Username}! 🥳`)
+        activeplayer.score++
+        throw new Error("End's the game!");
+      }
 
     }
+  }
 
+  const playRound = function (e) {
 
-
-
-
-    let activeplayer = players[0];
-
-    let switchplayers = () => {
-      activeplayer = activeplayer == players[0] ? players[1] : players[0]
-    }
-
-    let newTurn = () => {
-      switchplayers();
-      alert(`Its ${activeplayer.Username}'s go!`)
-    }
-
-
-
-    const droptkn = function (e) {
-
-      const coordinates = e.target.coordinates
-      const [row, col] = coordinates
-      let box = Gameboard[row][col]
-      if (box.getvalue() === null) {
-        box.marker(activeplayer);
-      } else { droptkn() }
-
-      // CHECK WIN COND HERE
-
-      /*activeplayer needed in arg?*/
-      // box.target.src = Gamecontroller.activeplayer.markerimg
-
-    }
-
-    return { droptkn, startGame, newTurn, players, activeplayer, winCheck, playRound }
-
-
-  })();
-
-
-
-  function Cell() {
-
-    let value = null
-
-    const getvalue = () => value;
-
-    const marker = (player) => value = player.marker
-
-    return { getvalue, marker }
+    droptkn(e)
+    screenSelector.markeranimationsX(e, activeplayer)
+    winCheck()
+    // possibly put draw check?
+    newTurn()
 
   }
 
 
-  // Screen selector
 
-  const screenSelector = (function () {
 
-    const tictacBoxes = document.querySelectorAll('.box')
 
-    let k = 0
+  let activeplayer = players[0];
 
-    for (let i = 0; i < 3; i++) {
+  let switchplayers = () => {
+    activeplayer = activeplayer == players[0] ? players[1] : players[0]
+  }
 
-      for (let j = 0; j < 3; j++) {
+  let newTurn = () => {
+    switchplayers();
+    alert(`Its ${activeplayer.Username}'s go!`)
+  }
 
-        tictacBoxes[k].coordinates = [i, j]
-        k++
-      }
 
+
+  const droptkn = function (e) {
+
+    const coordinates = e.target.coordinates
+    const [row, col] = coordinates
+    let box = Gameboard[row][col]
+    if (box.getvalue() === null) {
+      box.marker(activeplayer);
+    } else { droptkn() }
+
+    // CHECK WIN COND HERE
+
+    /*activeplayer needed in arg?*/
+    // box.target.src = Gamecontroller.activeplayer.markerimg
+
+  }
+
+  return { droptkn, newTurn, players, activeplayer, winCheck, playRound, makeBoard, startGame }
+
+
+})();
+
+
+
+function Cell() {
+
+  let value = null
+
+  const getvalue = () => value;
+
+  const marker = (player) => value = player.marker
+
+  return { getvalue, marker }
+
+}
+
+
+// Screen selector
+
+const screenSelector = (function () {
+
+  // Need to have the button be unclickable after this idk?
+
+  const tictacBoxes = document.querySelectorAll('.box')
+
+  let k = 0
+
+  for (let i = 0; i < 3; i++) {
+
+    for (let j = 0; j < 3; j++) {
+
+      tictacBoxes[k].coordinates = [i, j]
+      k++
     }
+    // this will assign co-ordinates to the elements associated with the gameboard
+  }
+
+  const allowClicks = function () {
+
 
     tictacBoxes.forEach(box => {
-      box.addEventListener('click', test = function (e) {
-        const coordinates = e.target.coordinates
+      box.addEventListener('click', function clickBoxEvent(e) {
         Gamecontroller.playRound(e)
-
       })
 
     })
 
-    const markeranimationsX = function (e, activeplayer) {
-      // const svg = document.createElement('svg'); //Get svg element
-      // const newElement = document.createElementNS("http://www.w3.org/2000/svg", 'path'); //Create a path in SVG's namespace
-      // newElement.setAttribute("d","m194.800781 164.769531 128.210938-128.214843c8.34375-8.339844 8.34375-21.824219 0-30.164063-8.339844-8.339844-21.824219-8.339844-30.164063 0l-128.214844 128.214844-128.210937-128.214844c-8.34375-8.339844-21.824219-8.339844-30.164063 0-8.34375 8.339844-8.34375 21.824219 0 30.164063l128.210938 128.214843-128.210938 128.214844c-8.34375 8.339844-8.34375 21.824219 0 30.164063 4.15625 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921875-2.089844 15.082031-6.25l128.210937-128.214844 128.214844 128.214844c4.160156 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921874-2.089844 15.082031-6.25 8.34375-8.339844 8.34375-21.824219 0-30.164063zm0 0"); //Set path's data
-      // newElement.style.width = "329pt"
-      // svg.appendChild(newElement);
-      // e.target.appendChild(svg)
+  }
 
-      const node = e.target
-      const nodeImg = document.createElement('img')
-      nodeImg.classList.add("objectfit")
-      nodeImg.src = activeplayer.markerImg
-      node.appendChild(nodeImg)
-    }
+  const test = () => console.log('test')
+
+  const markeranimationsX = function (e, activeplayer) {
+    const node = e.target
+    const nodeImg = document.createElement('img')
+    nodeImg.classList.add("objectfit")
+    nodeImg.src = activeplayer.markerImg
+    node.appendChild(nodeImg)
+  }
 
 
-    const updateScreen = () => x
+  const updateScreen = () => x
 
-    return{markeranimationsX}
+  const startGame = () => Gamecontroller.startGame()
+  
+  const startButton = document.querySelector('#startBtn')
+  startButton.addEventListener('click', startGame)
 
-  })()
 
+  return { markeranimationsX, allowClicks, test, startGame }
 
+})()
